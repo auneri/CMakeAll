e862881dd94c2e53967f615d19f4aad97562386a
Ali Uneri <ali.uneri@jhu.edu>
2013-12-18 12:59:36 -0500

    Increment version to 1.1, offer SameMajorVersion compatibility

--- CMakeAllConfigVersion.cmake
+++ CMakeAllConfigVersion.cmake
@@ -1,28 +1,21 @@
-set(PACKAGE_VERSION "1.0")
+set(PACKAGE_VERSION "1.1")
 
-if("${PACKAGE_VERSION}" MATCHES "^([0-9]+\\.[0-9]+\\.[0-9]+)\\..*")  # strip the tweak version
-  set(CVF_VERSION_NO_TWEAK "${CMAKE_MATCH_1}")
-else()
-  set(CVF_VERSION_NO_TWEAK "${PACKAGE_VERSION}")
-endif()
-
-if("${PACKAGE_FIND_VERSION}" MATCHES "^([0-9]+\\.[0-9]+\\.[0-9]+)\\..*")  # strip the tweak version
-  set(REQUESTED_VERSION_NO_TWEAK "${CMAKE_MATCH_1}")
-else()
-  set(REQUESTED_VERSION_NO_TWEAK "${PACKAGE_FIND_VERSION}")
-endif()
-
-if("${REQUESTED_VERSION_NO_TWEAK}" STREQUAL "${CVF_VERSION_NO_TWEAK}")
-  set(PACKAGE_VERSION_COMPATIBLE TRUE)
-else()
+if("${PACKAGE_VERSION}" VERSION_LESS "${PACKAGE_FIND_VERSION}" )
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
-endif()
+else()
+  if("${PACKAGE_VERSION}" MATCHES "^([0-9]+)\\.")
+    set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
+  else()
+    set(CVF_VERSION_MAJOR "${PACKAGE_VERSION}")
+  endif()
 
-if("${PACKAGE_FIND_VERSION}" STREQUAL "${PACKAGE_VERSION}")
-  set(PACKAGE_VERSION_EXACT TRUE)
-endif()
+  if("${PACKAGE_FIND_VERSION_MAJOR}" STREQUAL "${CVF_VERSION_MAJOR}")
+    set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  else()
+    set(PACKAGE_VERSION_COMPATIBLE FALSE)
+  endif()
 
-# if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:
-if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "" OR "8" STREQUAL "")
-  return()
+  if("${PACKAGE_FIND_VERSION}" STREQUAL "${PACKAGE_VERSION}")
+    set(PACKAGE_VERSION_EXACT TRUE)
+  endif()
 endif()
